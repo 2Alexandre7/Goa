2491f780b86c0f05837b68461850e4e798fb6637
2Alexandre7 <abashidze.alexsandre2012@gmail.com>
2024-10-20 12:45:17 +0400

    lection_2 work

---
+++ lesson2.py
@@ -0,0 +1,133 @@
+from turtle import *
+
+
+color("red")
+width(5)
+speed(30)
+
+forward(340)
+
+penup()
+goto(0, 0)
+pendown()
+
+left(180)
+forward(340)
+
+right(90)
+forward(230)
+
+right(90)
+forward(70)
+
+right(90)
+forward(230)
+
+penup()
+goto(-270, 170)
+pendown()
+
+left(90)
+forward(70)
+
+right(90)
+forward(170)
+
+penup()
+goto(-200, 170)
+pendown()
+
+right(180)
+forward(60)
+
+right(90)
+forward(70)
+
+right(90)
+forward(230)
+
+penup()
+goto(-130, 170)
+pendown()
+
+left(90)
+forward(70)
+
+right(90)
+forward(170)
+
+penup()
+goto(-60, 170)
+pendown()
+
+left(180)
+
+
+penup()
+goto(330, 0)
+pendown()
+
+forward(230)
+
+left(90)
+forward(70)
+
+left(90)
+forward(230)
+
+penup()
+goto(260, 170)
+pendown()
+
+right(90)
+forward(70)
+
+left(90)
+forward(170)
+
+penup()
+goto(190, 170)
+pendown()
+
+left(180)
+forward(60)
+
+left(90)
+forward(70)
+
+left(90)
+forward(230)
+
+penup()
+goto(120, 170)
+pendown()
+
+right(90)
+forward(70)
+
+left(90)
+forward(170)
+
+penup()
+goto(-3, 0)
+pendown()
+
+left(180)
+forward(170)
+
+left(90)
+forward(60)
+
+penup()
+goto(-3, 170)
+pendown()
+
+right(180)
+forward(60)
+
+penup()
+goto(-3, -100)
+pendown()
+
+
+exitonclick()
